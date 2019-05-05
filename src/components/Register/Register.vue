a357f21656d45884517e18cfb95d318fa1cd87b8
16341018 <38913031+ezlaor@users.noreply.github.com>
2019-05-05 18:06:16 +0800

    demo for register

--- src/components/Register/Register.vue
+++ src/components/Register/Register.vue
@@ -1,19 +1,179 @@
 <template>
-	
+<el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="regist-box">
+  <span>请填写基本信息</span>
 
+  <el-form-item label="学号" prop="number">
+    <el-input v-model="ruleForm.number"></el-input>
+  </el-form-item>
+  <el-form-item label="姓名" prop="name">
+    <el-input v-model="ruleForm.name"></el-input>
+  </el-form-item>
+
+  <el-form-item label="年龄/年级" required>
+    <el-col :span="5">
+      <el-form-item  prop="age">
+        <el-input  type="text" placeholder="年龄" v-model="ruleForm.age" style="width: 100%;"></el-input>
+      </el-form-item>
+    </el-col>
+    <el-col class="line" :span="5"></el-col>
+    <el-col :span="5">
+      <el-form-item prop="semester">
+        <el-select  placeholder="选择年级" v-model="ruleForm.semester" style="width: 100%;">
+            <el-option label="大一" value="1"></el-option>
+            <el-option label="大二" value="2"></el-option>
+            <el-option label="大三" value="3"></el-option>
+            <el-option label="大四" value="4"></el-option>
+            <el-option label="研一" value="5"></el-option>
+            <el-option label="研二" value="6"></el-option>
+        </el-select>
+      </el-form-item>
+    </el-col>
+  </el-form-item>
+ 
+  <el-form-item label="性别" prop="resource" >
+    <el-radio-group v-model="ruleForm.resource">
+      <el-radio label="男"></el-radio>
+      <el-radio label="女"></el-radio>
+    </el-radio-group>
+  </el-form-item>
+
+<el-form-item label="专业" prop="major">
+    <el-input  v-model="ruleForm.major" placeholder="请输入专业"></el-input>
+  </el-form-item>
+  
+
+<el-form-item label="密码" prop="pass">
+    <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+  </el-form-item>
+
+  <el-form-item label="确认密码" prop="checkPass">
+    <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+  </el-form-item>
+    
+    <el-form-item label="常用手机号" prop="phone_num">
+    <el-input  v-model="ruleForm.phone_num" autocomplete="off"></el-input>
+  </el-form-item>
+    
+    <el-form-item label="验证码" prop="code" >
+    <el-input  v-model="ruleForm.code" autocomplete="off"></el-input>
+  </el-form-item>
+
+  <el-form-item>
+    <el-button class="regist-button" type="success" @click="submitForm('ruleForm')" >注册</el-button>
+    
+  </el-form-item>
+
+     <el-link class="link" type="success" @click="onLogin()">已有账号？立即登录！</el-link>
+</el-form>
 </template>
 
 <script>
-export default {
-  name: 'Register'
-  /*data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    data() {
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          if (this.ruleForm.checkPass !== '') {
+            this.$refs.ruleForm.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.ruleForm.pass) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };
+      return {
+        ruleForm: {
+          number: '',
+          name:'',
+          age: '',
+          semester: '',
+          major:'',
+          pass:'',
+          checkPass:'',
+          phone_num:'',
+          code:''
+        },
+        rules: {
+          number: [
+            { required: true, message: '请输入6位学号', trigger: 'change' },
+            { min: 6, max: 6, message: '请输入6位学号', trigger: 'change' }
+          ],
+          name:[
+            { required: true, message: '请输入姓名', trigger: 'change' }
+          ],
+          
+          age: [
+            {  required: true, message: '请输入年龄', trigger: 'change' }
+          ],
+          semester: [
+            {  required: true, message: '请选择年级', trigger: 'change' }
+          ],
+          major:[
+            {required: true, message: '请输入专业', trigger: 'change'}
+          ],
+         pass: [
+            { validator: validatePass, trigger: 'blur' }
+          ],
+          checkPass: [
+            { validator: validatePass2, trigger: 'blur' }
+          ],
+           phone_num: [
+            { required: true, message: '请输入手机号', trigger: 'change' }
+          ],
+          code:[
+            {required: true, message: '请输入验证码', trigger: 'change'}
+          ]
+       
+        }
+      };
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      
+      onLogin(){
+          this.$router.push("/Signin");
+      }
     }
-  }*/
-}
+  }
 </script>
-
 <style scoped>
+  .regist-box{
+    margin: 180px auto;
+    width: 500px;
+    height: 700px;
+    padding: 0px 50px 20px 35px;
+     border-radius: 5px;
+    -webkit-border-radius: 5px;
+    -moz-border-radius: 5px;
+    box-shadow: 0 0 25px #909399;
+  }
+  .regist-button{
+    position: relative;
+     left: 60px;
+    width: 60%;
 
+  }
+
+  .link{
+    
+     position: relative;
+     left: 200px;
+  }
 </style>
