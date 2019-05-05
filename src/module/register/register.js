dd9e159a44554add474bcf2fbf8e7b5a9f5b9f08
16341018 <38913031+ezlaor@users.noreply.github.com>
2019-05-05 18:08:41 +0800

    Update register.js

--- src/module/register/register.js
+++ src/module/register/register.js
@@ -1,9 +1,21 @@
-import Vue from 'vue'
+/*import Vue from 'vue'
 import App from './App'
 
-/* eslint-disable no-new */
+
 new Vue({
   el: '#app',
   template: '<App/>',
   components: { App }
+})
+ */
+import Vue from 'vue'
+import Register from 'components/Register/Register'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI);
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<Register/>',
+  components: { Register }
 })
