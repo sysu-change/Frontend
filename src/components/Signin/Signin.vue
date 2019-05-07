3fbe980baf4c4935953adc4524f01a5244317da8
16341018 <38913031+ezlaor@users.noreply.github.com>
2019-05-07 10:42:53 +0800

    Update Signin.vue

--- src/components/Signin/Signin.vue
+++ src/components/Signin/Signin.vue
@@ -1,29 +1,37 @@
 <template>
-	<el-container>
-		<el-header></el-header>
-		<el-main>
-			<el-form ref="loginForm" :model="form" :rules="rules" label-width="80px" class="login-box">
-				<h3 class="login-title" >登录</h3>
-				<el-divider class="line1" content-position="center"><span class="text">开始你的快乐之旅吧</span></el-divider>
-        <div class="former">
-          <div class="phone_num">
-            <el-input  placeholder="请输入手机号" @input="checkThePhone" v-model="input1" :disabled="false" ></el-input>
-            <span class="Err">{{ErrPhone}}</span>
-          </div>
-          <div class="pass_word">
-            <el-input  placeholder="请输入密码" @change="checkThePassword" v-model="input2" :disabled="false" show-password></el-input>
-            <span class="Err">{{ErrPassword}}</span>
-          </div>
-          <div class="pass_code">
-            <el-input  placeholder="请输入验证码" @change="checkTheMessage" v-model="input3" :disabled="false" ></el-input>
-            <span class="Err">{{ErrMessage}}</span>
-          </div>
-        </div>
-				<el-button type="success" class="login_button" @click="SigninforUser">登录</el-button>
-			</el-form>
-		</el-main>
-		<el-footer></el-footer>
-	</el-container>
+	
+			<div>
+    <el-form ref="loginForm" :model="form" status-icon :rules="rules" label-width="80px" class="login-box" >
+      <h3 class="login-title">欢迎登录</h3>
+      <el-form-item label="手机号" prop="phone_num" >
+        <el-input type="text" placeholder="请输入手机号" @input="checkThePhone"  v-model="form.phone_num"/>
+        
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" placeholder="请输入密码" v-model="form.password"/>
+      </el-form-item>
+      <el-form-item label="验证码" prop="code">
+        <el-input type="text" placeholder="请输入验证码" v-model="form.code"/>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="submit" type="success" v-on:click="onSubmit('loginForm')">登录</el-button>
+      </el-form-item>
+
+       <el-link class="link" type="success" href="register.html" >还没有账号？立即注册！</el-link>
+    </el-form>
+
+    <el-dialog
+      title="温馨提示"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose">
+      <span>请输入正确信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+	
 </template>
 
 
@@ -31,37 +39,69 @@
 export default{
 	el: '#app',
 	data() {
-		return {
-			input1: '',
-			input2: '',
-      input3: '',
-      ErrMessage:'...',
-      ErrPhone:'...',
-      ErrPassword:'...'
+    var validatePass = (rule, value, callback) => {
+       if (!value) {
+          return callback(new Error('手机号不能为空'));
+        }
+        setTimeout(() => {
+          var reg=/^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|17[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/;
+         if(!reg.test(this.form.phone_num)) {
+            callback(new Error('请输入正确格式'));
+        }else {
+              callback();
+            }
+          
+        }, 1000);
+      };
+
+		return { 
+			    form: {
+          phone_num: '',
+          password: '',
+          code:''
+        },
+
+           
+
+        rules: {
+          phone_num: [
+            
+            {validator: validatePass, trigger: 'blur' }
+          ],
+          password: [
+            {required: true, message: '密码不可为空', trigger: 'blur'}
+          ],
+          code:[
+            { required: true, message: '验证码不可为空', trigger: 'blur'}
+        ]
+
+        },
+
+        // 对话框显示和隐藏
+        dialogVisible: false
 		}
 	},
 	methods:{
     Sign:function() {
-      alert(this.input1+" "+this.input2+" "+this.input3);
+      alert(this.phone_num+" "+this.password+" "+this.code);
     },
-    checkThePhone:function(){
-        var reg=/^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/;
-        if(!reg.test(this.input1)) {
-          this.ErrPhone="请输入正确的格式"; 
-        }
-        else this.ErrPhone="正确";
-    },
-    checkThePassword:function() {
-        if(this.input2==='') this.ErrPassword="请输入密码";
-        else this.ErrPassword="...";
-    },
-    checkTheMessage:function() {
-        if(this.input3==='') this.ErrMessage="请输入验证码";
-        else this.ErrMessage="...";
-    },
+    
+    onSubmit(formName) {
+        // 为表单绑定验证功能
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            // 使用 vue-router 路由到指定页面，该方式称之为编程式导航
+            this.$router.push("/index");
+          } else {
+            this.dialogVisible = true;
+            return false;
+          }
+        });
+      },
+
     SigninforUser:function(){
-       this.$http.jsonp('http://localhost:8082/module/login',{phone_num:this.input1,password:this.input2}).then(function(res){
-          if(res.code===404) {alert("账户或密码错误");this.input1='';this.input2=''; }
+       this.$http.jsonp('http://localhost:8082/module/login',{phone_num:this.phone_num,password:this.password}).then(function(res){
+          if(res.code===404) {alert("账户或密码错误");this.phone_num='';this.password=''; }
           else if(res.data.code==200) {
             alert(res.data.msg);
           }
@@ -77,103 +117,46 @@ export default{
 
 <style scoped>
   
-  .el-header {
-    background-color: #00b38a;
-    color: #333;
-    height: 20vh;
-    width: 100%;
-    
+  .link{
+    top: -10px;
+     position: relative;
+     left: 15px;
   }
-
-  .el-footer{
-    background-color: #999999;
-    color:#fcfcfc;
-    width: 100%;
-    height: 20vh;
-  }
-  
-  .el-main {
-    background-color: #fcfcfc;
-    color: #333;
-    text-align: center;
-    height: 60vh;
-  }
-   .el-container {
-    background-color: #00b384;
-    margin-bottom: 0px;
-    
-    width: 100%;
-    height: 100vh;
-    position: absolute;
-  }
-   
-
-   
   
   .login-box {
-    position: absolute;
     border: 1px solid #DCDFE6;
     width: 400px;
-    height:350px;
-    left: 35%;
-    top: 125px;
+    height:300px;
+    margin: 180px auto;
+    padding: 0px 35px 20px 35px;
+    border-radius: 5px;
+    -webkit-border-radius: 5px;
+    -moz-border-radius: 5px;
     box-shadow: 0 0 25px #909399;
   }
-  
-  .pass_num{
-    position: relative;
-    left: 5px;
-    width:200px;
-  }
-  
-  .phone_word{
-    position: relative;
-    left: 5px;
-    top:80px;
-    width:200px;
-  }
 
-  .pass_code{
+  .submit{
     position: relative;
-    width:150px;
+    width: 200px;
+    left: -25px;
   }
+  
+ 
 
 .login-title {
-    text-align: center;
+    text-align: left;
     font: 1em sans-serif;
     color: #00b38a;
   }
 
 .login_button{
-  position: relative;
-  left: 10px;
-  width: 150px;
-  height: 40px;
-  top:30px;
-}
-
-.bar{
   position: absolute;
-  left: 50px;
+  left: 150px;
   width: 150px;
   height: 40px;
   top: 260px;
 }
 
-.former{
-  background-color:#fcfcfc;
-}
 
-.text{
-  font-size: 10px;
-  color: #00b38a;
-}
 
-.Err{
-  display: block;
-  text-align: left;
-  color:red;
-  font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
-  opacity:0.5;
-}
 </style>
